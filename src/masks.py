66155836166af66d85ceeb65b46b2ed80ee96fd2
dmitry padusev <164377839+fox1kq@users.noreply.github.com>
2025-02-11 18:18:18 +0300

    /

--- src/masks.py
+++ src/masks.py
@@ -6,3 +6,8 @@ def get_mask_number_card(card: str) -> str:
         new_card[:4] + " " + new_card[4:8] + " " + new_card[8:12] + " " + new_card[12:]
     )
     return card_under_mask
+
+def get_mask_account(card: str) -> str:
+    """Функция, принимающая номер карты и возвращающая номер под маской,
+    где видны последние 4 цифры, а перед ними 2 звездочки"""
+    return "**" + card[16:]
