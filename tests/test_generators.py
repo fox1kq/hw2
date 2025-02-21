32b6cb51a5fd35e8215087de579b4e2916959871
Padusev.D <fox1kqq@gmail.com>
2025-02-21 12:50:46 +0300

    bug fixes + add html cov tests

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 
 transactions = [
@@ -49,43 +51,80 @@ transactions = [
 ]
 
 
-def test_filter_by_currency() -> None:
+@pytest.fixture
+def fixture_filter_by_currency() -> list:
+    return [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702",
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188",
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+            "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод с карты на карту",
+            "from": "Visa Classic 6831982476737658",
+            "to": "Visa Platinum 8990922113665229",
+        },
+    ]
+
+
+def test_filter_by_currency(fixture_filter_by_currency: list) -> None:
     generator = filter_by_currency(transactions, "USD")
-    assert next(generator) == {
-        "id": 939719570,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод организации",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702",
-    }
-    assert next(generator) == {
-        "id": 142264268,
-        "state": "EXECUTED",
-        "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод со счета на счет",
-        "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188",
-    }
+    assert list(generator) == fixture_filter_by_currency
 
 
-def test_transaction_descriptions() -> None:
+@pytest.fixture
+def fixture_transaction_descriptions() -> list:
+    return [
+        "Перевод организации",
+        "Перевод со счета на счет",
+        "Перевод со счета на счет",
+        "Перевод с карты на карту",
+        "Перевод организации",
+    ]
+
+
+def test_transaction_descriptions(fixture_transaction_descriptions: list) -> None:
     generator = transaction_descriptions(transactions)
-    assert next(generator) == "Перевод организации"
-    assert next(generator) == "Перевод со счета на счет"
-    assert next(generator) == "Перевод со счета на счет"
-    assert next(generator) == "Перевод с карты на карту"
-    assert next(generator) == "Перевод организации"
+    assert list(generator) == fixture_transaction_descriptions
 
 
-def test_card_number_generator() -> None:
+@pytest.fixture
+def fixture_card_number_generator() -> list:
+    return [
+        "0000 0000 0000 0001",
+        "0000 0000 0000 0002",
+        "0000 0000 0000 0003",
+        "0000 0000 0000 0004",
+        "0000 0000 0000 0005",
+    ]
+
+
+@pytest.fixture
+def fixture_card_number_generator_zero() -> list:
+    return [
+        "0000 0000 0000 0000",
+    ]
+
+
+def test_card_number_generator(fixture_card_number_generator: list, fixture_card_number_generator_zero: list) -> None:
     generator = card_number_generator(1, 5)
-    assert next(generator) == "0000 0000 0000 0001"
-    assert next(generator) == "0000 0000 0000 0002"
-    assert next(generator) == "0000 0000 0000 0003"
-    assert next(generator) == "0000 0000 0000 0004"
-    assert next(generator) == "0000 0000 0000 0005"
+    assert list(generator) == fixture_card_number_generator
     generator = card_number_generator(0, 0)
-    assert next(generator) == "0000 0000 0000 0000"
+    assert list(generator) == fixture_card_number_generator_zero
