c22b936f8c450f041c5ebe667a9f907ebaec81ba
dmitry padusev <164377839+fox1kq@users.noreply.github.com>
2025-03-27 21:26:54 +0300

    new functions

---
+++ src/utils.py
@@ -0,0 +1,14 @@
+import json
+import os
+
+
+def load_financial_transactions(path_to_file):
+    if not os.path.exists(path_to_file):
+        return []
+
+    with open(path_to_file, "r", encoding="utf-8") as json_file:
+        data = json.load(json_file)
+        if isinstance(data, list):
+            return data
+        else:
+            return "Ошибка"
