1195cec75b4bcc5f414a29a6d90b7e10f8f36a77
dmitry padusev <164377839+fox1kq@users.noreply.github.com>
2025-03-28 11:52:41 +0300

    logging

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,36 @@
 import json
 import os
+import logging
+
+# Основная конфигурация logging
+logging.basicConfig(level=logging.DEBUG,
+                    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    filename = r'D:\PythonProjects\hw2\logs\utils.log',
+                    filemode = 'w',
+                    encoding = 'utf-8')
+
+
+load_financial_transactions_logger = logging.getLogger("load_financial_transactions")
+
+
 
 
 def load_financial_transactions(path_to_file):
+    load_financial_transactions_logger.info("Начало выполнения функции")
+
     if not os.path.exists(path_to_file):
+        load_financial_transactions_logger.warning("Файл не найден")
         return []
 
     with open(path_to_file, "r", encoding="utf-8") as json_file:
         data = json.load(json_file)
+
         if isinstance(data, list):
+            load_financial_transactions_logger.info("Успешное выполнение")
             return data
+
         else:
+            load_financial_transactions_logger.warning("Файл не содержит списка")
             return []
+
+print(load_financial_transactions("blabla"))
