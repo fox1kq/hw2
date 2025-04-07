3632a588c1f044263dad7a49f27461e0f3d3c3b6
Padusev.D <735@gmail.com>
2025-04-07 15:57:08 +0300

    csv, pandas

--- src/reading.py
+++ src/reading.py
@@ -1,13 +1,15 @@
 import csv
+from typing import Any
 import pandas as pd
 
-def reading_from_csv(path):
+
+def reading_from_csv(path: str) -> Any:
     with open(path, encoding="utf8") as file:
         reader = csv.reader(file, delimiter=";")
         for row in reader:
             print(row)
 
 
-def reading_from_excel(path):
+def reading_from_excel(path:str) -> Any:
     df = pd.read_excel(path)
     return df
